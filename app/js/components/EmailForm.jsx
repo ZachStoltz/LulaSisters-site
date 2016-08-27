def1cb9239e82852cfa3def1b4104af43a7076ea
Zach Stoltz <zach@stoltz.io>
2016-08-27 13:45:44 -0400

    footer almost complete

--- app/js/components/EmailForm.jsx
+++ app/js/components/EmailForm.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 export default (props) => (
-  <form className={props.className}>
-    <label htmlFor="name-input">Name</label>
-    <input type="text" name="name-input" placeholder="name" required />
-    <label htmlFor="email-input">Email</label>
-    <input type="email" name="email-input" placeholder="email" required />
-    <label htmlFor="textarea">Message</label>
-    <textarea name="textarea" rows="5" placeholder="message" required />
-    <button className="btn btn-submit btn-small">Submit</button>
-  </form>
+  <div className={props.className}>
+    <h4>Quick Response to Dicia & Erica</h4>
+    <form>
+      <div className="input-group">
+        <label htmlFor="name-input">name:</label>
+        <input type="text" name="name-input" placeholder="name" required />
+      </div>
+      <div className="input-group">
+        <label htmlFor="email-input">email:</label>
+        <input type="email" name="email-input" placeholder="email" required />
+      </div>
+      <div className="input-group textarea">
+        <label htmlFor="textarea">message:</label>
+        <textarea name="textarea" rows="5" placeholder="message" required />
+      </div>
+      <button className="btn btn-submit btn-small">Submit</button>
+    </form>
+  </div>
 );
--- app/js/components/Footer.jsx
+++ app/js/components/Footer.jsx
@@ -9,14 +9,14 @@ export default () => (
         PHOTOS FROM INSTAGRAM
       </div>
       <div>
-        <h5>Getting Started</h5>
+        <h4>Getting Started</h4>
         <ul>
           <a><li>How to shop</li></a>
           <a><li>How to shop</li></a>
         </ul>
       </div>
       <div>
-        <h5>Wanna Talk?</h5>
+        <h4>Wanna Talk?</h4>
         <p>
           Pug synth paleo vinyl messenger bag,
           heirloom gochujang retro vice meggings
@@ -24,7 +24,16 @@ export default () => (
           lumbersexual distillery. Retro gluten-free irony, meh
           farm-to-table quinoa cliche drinking vinegar brunch cold-pressed.
         </p>
-        <span>@</span><a href="mailto:erica@stoltz.io?cc=dicia@marguccio.com&amp;subject=Heyo%20Lets%20talk%20Lularoe">lulasistersdiciaanderica@gmail.com</a>
+        <div className="email">
+          <i className="fa fa-at" aria-hidden="true" />
+          <a href="mailto:erica@stoltz.io?cc=dicia@marguccio.com&amp;subject=Heyo%20Lets%20talk%20Lularoe">
+            lulasistersdiciaanderica@gmail.com
+          </a>
+        </div>
+        <div className="fb">
+          <i className="fa fa-facebook" aria-hidden="true" />
+          <a href="#">LuLaSisters: Dicia & Erica</a>
+        </div>
       </div>
       <EmailForm className="email-form" />
     </div>
